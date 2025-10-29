91fa02824ce27418e0fab0f8544543cab719c808
FOLINK <1602347500@qq.com>
2025-10-29 11:22:37 +0800

    Sort : clean unused files

--- external/jmcomic - 副本/readme.md
+++
@@ -1,5 +0,0 @@
-# koishi-plugin-jmcomic
-
-[![npm](https://img.shields.io/npm/v/koishi-plugin-jmcomic?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-jmcomic)
-
-
--- external/jmcomic - 副本/src/main.py
+++
@@ -1,7 +0,0 @@
-# from jmcomic import JmOption
-import jmcomic
-import sys
-
-number = sys.argv[1]
-option = jmcomic.create_option_by_file('external\jmcomic\src\option.yml')
-option.download_album(number)
